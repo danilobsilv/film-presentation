400dca4ea718696f2aee06d5b771126b5de105f7
Danilo Bruno <danilobsilv@gmail.com>
2023-05-01 18:41:52 -0400

    function to get data from the api 22

---
+++ backend/api/getMovies2022.js
@@ -0,0 +1,18 @@
+function getBestMovies2022(apiKey, fs) {
+      const url = `https://api.themoviedb.org/3/discover/movie?api_key=${apiKey}&language=pt-BR&sort_by=revenue.desc&include_adult=false&include_video=false&page=1&primary_release_year=2022`;
+      
+      return fetch(url)
+      .then(response => response.json())
+      .then(data => {
+        const jsonResult = JSON.stringify(data.results, null, 4);
+        fs.writeFileSync('backend/json/movieData2022.json', jsonResult);
+        return data.results;
+      })
+      .catch(error => {
+        console.error(error);
+        return [];
+        }); 
+      }
+      
+      
+      module.exports = {getBestMovies2022};
