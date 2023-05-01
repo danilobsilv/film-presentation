facaf6daab95700b2a6beb7c1466954f10f07cda
Danilo Bruno <danilobsilv@gmail.com>
2023-05-01 18:17:13 -0400

    function to get the data from the api

--- backend/api/getMovies2021.js
+++ backend/api/getMovies2021.js
@@ -1,32 +1,19 @@
-const fs = require('fs');
 
-
-async function getMovies(apiKey, releaseYear, page = 1, fileName = 'movieData.json') {
-
-  function getApiKey() {
-    const apiKeyPath = 'config/api_key.json';
-    const apiKeyData = fs.readFileSync(apiKeyPath);
-    const apiKey = JSON.parse(apiKeyData).API_KEY;
-    return apiKey;
-  }  
-
-  const api_key = getApiKey();
-
-  const url = `https://api.themoviedb.org/3/discover/movie?api_key=${apiKey}&language=pt-BR&sort_by=popularity.desc&include_adult=false&include_video=false&page=1&primary_release_year=${releaseYear}`;
-
-  try {
-    const response = await fetch(url);
-    const data = await response.json();
-    const movies = data.results;
-
-    const json = JSON.stringify(movies, null, 4);
-    fs.writeFileSync(`backend/json/${fileName}`, json, { encoding: 'utf8' });
-
-    return movies;
-  } catch (error) {
+function getBestMovies2021(apiKey, fs) {
+  const url = `https://api.themoviedb.org/3/discover/movie?api_key=${apiKey}&language=pt-BR&sort_by=revenue.desc&include_adult=false&include_video=false&page=1&primary_release_year=2021`;
+  
+  return fetch(url)
+  .then(response => response.json())
+  .then(data => {
+    const jsonResult = JSON.stringify(data.results, null, 4);
+    fs.writeFileSync('backend/json/movieData2022.json', jsonResult);
+    return data.results;
+  })
+  .catch(error => {
     console.error(error);
     return [];
+    }); 
   }
-}
-
-module.exports = { getMovies };
+  
+  
+  module.exports = {getBestMovies2021};
