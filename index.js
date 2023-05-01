3a22549bebb8e419f886951490439f82769ebd06
Danilo Bruno <danilobsilv@gmail.com>
2023-05-01 18:44:29 -0400

    testing

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
-const  { getBestMovies2021 } = require( './backend/api/getMovies2021');
+const { getBestMovies2021 } = require( './backend/api/getMovies2021');
+const  { getBestMovies2022 } = require( './backend/api/getMovies2022');
 const fs = require('fs');
 
 const apiKey = "6f8f868defa0de80c57749ed0f736f66"
 
 getBestMovies2021(apiKey, fs);
+getBestMovies2022(apiKey, fs);
