d313e53e2a749a312f5f893226137aed1e6daebf
Danilo Bruno <danilobsilv@gmail.com>
2023-04-30 21:05:42 -0400

    tests

--- index.js
+++ index.js
@@ -1,4 +1,18 @@
-const { getMovieData } = require('./backend/api/getData');
+// // const { getMovieData } = require('./backend/api/getData');
 
-// Chama a função getMovieData para obter os dados do filme
-getMovieData();
+// // Chama a função getMovieData para obter os dados do filme
+// getMovieData();
+
+
+// const { getMovies } = require('./backend/api/getMovies2021');
+
+
+
+// getMovies(apiKey, releaseYear)
+//   .then(movies => {
+//     for (const movie of movies) {
+//       console.log(`${movie.title} (${movie.release_date}): ${movie.overview}`);
+//     }
+//   });
+
+// getMovies(apiKey, releaseYear);
