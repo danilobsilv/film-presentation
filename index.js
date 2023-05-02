ab3551e52e8b4138e2543231193088be01107682
Danilo Bruno <danilobsilv@gmail.com>
2023-05-02 18:35:44 -0400

    test

--- index.js
+++ index.js
@@ -2,12 +2,28 @@ const { getBestMovies2021 } = require( './backend/api/getMovies2021');
 const  { getBestMovies2022 } = require( './backend/api/getMovies2022');
 const { getTopRatedMovies } = require('./backend/api/getTopRatedMovies'); 
 const {getWorstRatedMoviesEver} = require('./backend/api/getWorstRatedMoviesEver')
-const fs = require('fs');
 
+const fs = require('fs');
 const apiKey = "6f8f868defa0de80c57749ed0f736f66"
+
+// const express = require('express');
+// const app = express();
+// const serverRoutes = require('./routes/server.js');
+
+// serverRoutes(app);
 
 
 // getWorstRatedMoviesEver(apiKey, fs);
 // getTopRatedMovies(apiKey, fs)
-getBestMovies2021(apiKey, fs);
+// getBestMovies2021(apiKey, fs);
 // getBestMovies2022(apiKey, fs);
+
+const express = require('express');
+const app = express();
+const routes = require('./routes/server');
+
+app.use('public/pickCategorieScreen/pickCategorie.html', routes);
+
+app.listen(5500, () => {
+  console.log('Server running at http://localhost:5500');
+});
