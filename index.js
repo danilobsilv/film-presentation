45890acdd0ff736a0a62f9d74dfdcd0547dde986
Danilo Bruno <danilobsilv@gmail.com>
2023-05-01 23:09:50 -0400

    testing

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
 const { getBestMovies2021 } = require( './backend/api/getMovies2021');
 const  { getBestMovies2022 } = require( './backend/api/getMovies2022');
+const { getTopRatedMovies } = require('./backend/api/getTopRatedMovies'); 
+const {getWorstRatedMoviesEver} = require('./backend/api/getWorstRatedMoviesEver')
 const fs = require('fs');
 
 const apiKey = "6f8f868defa0de80c57749ed0f736f66"
 
-getBestMovies2021(apiKey, fs);
-getBestMovies2022(apiKey, fs);
+
+// getWorstRatedMoviesEver(apiKey, fs);
+// getTopRatedMovies(apiKey, fs)
+// getBestMovies2021(apiKey, fs);
+// getBestMovies2022(apiKey, fs);
