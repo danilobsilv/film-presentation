610700d0032f2abf672fe914aa3482c77d52366c
Danilo Bruno <danilobsilv@gmail.com>
2023-05-05 00:09:56 -0400

    to read the api key

--- utils/readApiKey.js
+++ utils/readApiKey.js
@@ -1,7 +1,7 @@
 function getApiKey() {
       const apiKeyPath = 'config/api_key.json';
       const apiKeyData = fs.readFileSync(apiKeyPath);
-      const apiKey = JSON.parse(apiKeyData).API_KEY;
-      return apiKey;
+      const apikey = JSON.parse(apiKeyData).API_KEY;
+      return apikey;
     }
     
