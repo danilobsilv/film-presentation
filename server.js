844770674caa9c82272c9392da6e506fd231338f
Danilo Bruno <danilobsilv@gmail.com>
2023-05-12 16:15:18 -0400

    fixing routes

--- server.js
+++ server.js
@@ -3,7 +3,6 @@ const path = require('path');
 const app = express();
 const cors = require('cors');
 
-//  to execute the script, "npm run dev"
 
 app.use(express.static('public'));
 app.use(express.urlencoded({ extended: true }));
@@ -20,41 +19,51 @@ app.get('/main', function(req, res) {
       return res.render("pickcategorie.html");
 });
 
+
 app.get("/login", function (req, res){
       res.render("loginview.html");
 });
+
 
 app.get("/login/registeruser", function (req, res){
       res.render("registeruser.html");
 })
 
+
 app.get("/", function (req, res){
       res.render("landingpage.html");
 });
+
 
 app.get("/main/session1", function(req, res) {
       res.render("bestmovies.html");
 });
 
+
 app.get("/main/session2", function(req, res){
       res.render("lowermoviescreen.html");
 })
+
 
 app.get("/main/session3", function(req, res){
       res.render("bestmovies2021.html");
 })
 
+
 app.get("/main/session4", function(req, res){
       res.render("bestmovies2022.html");
 })
+
 
 app.get("/filmesfavoritos", function(req, res){
       res.render("filmesfavoritos.html")
 });
 
+
 app.get("/sobremim", function(req, res){
       res.render("sobremim.html");
 });
+
 
 app.listen(3333, function(){
       console.log("SERVER IS RUNNING!\n")
